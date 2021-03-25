051a061a49864ec883ff9f830afcba74a3010088
Adriana <am20dipi@alum.siena.edu>
2021-03-25 18:46:06 -0400

    commit tree 620b20b81788f732295141b5d02c34b8d4485003

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,24 @@
 class Song < ActiveRecord::Base
   # add associations here
+  has_many :notes
+  belongs_to :artist
+  belongs_to :genre
+
+  def artist_name=(name)
+    self.artist = Artist.find_by_or_create_by(name: name)
+    self.save
+  end
+
+  def artist_name
+    self.artist.name if self.artist != nil
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre.name if self.genre !=nil
+  end
+  
 end
